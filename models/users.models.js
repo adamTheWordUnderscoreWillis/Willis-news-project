2a15d78fd2aadca00a9a70eb7cd7a29f76ab1c29
PrickWillis <adamthewordunderscorewillis@gmail.com>
2024-05-31 10:41:52 +0100

    creates an endpoint for users to find an array of users

---
+++ models/users.models.js
@@ -0,0 +1,15 @@
+const db = require("../db/connection")
+
+exports.fetchAllUsers = ()=>{
+    const queryStatement = `
+    SELECT * FROM users
+    ORDER BY username ASC
+    `
+    return db
+    .query(queryStatement)
+    .then(({rows})=>{
+        console.log(rows)
+        return rows
+    })
+
+}
